e6427c21ede26ff67fa35fae2c660a27f02b9333
MacStudent <macstudent@126-iMac06.local>
2019-02-14 19:45:43 -0500

    faculty

---
+++ Swift_Project/faculty.swift
@@ -0,0 +1,18 @@
+//
+//  faculty.swift
+//  Swift_Project
+//
+//  Created by MacStudent on 2019-02-14.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+
+class Faculty
+{
+    private var fid: Int = 0
+    private var fname: String = ""
+    private var lname: String = ""
+    private var dept: String = ""
+}
